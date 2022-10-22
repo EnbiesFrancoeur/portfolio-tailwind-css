a6b4fbc8746950e1a53c7da03212862f65204bbf
Nicolas Batara <enbies00@gmail.com>
2022-10-22 13:02:34 +0700

    ngerapihin

--- dist/script.js
+++ dist/script.js
@@ -54,3 +54,10 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
 } else {
     darkToggle.checked = false;
 }
+
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+} else {
+    document.documentElement.classList.remove('dark')
+}
